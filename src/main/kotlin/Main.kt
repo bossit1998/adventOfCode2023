4ba71574ceb8e1ab7ec399d223492d5be83e83f9
abdubosit <bossit5855@gmail.com>
2023-12-09 15:36:10 +0500

    day 8 and 9

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,6 +26,91 @@ val reset = "\u001b[0m"
 
 val dot = '.'
 
+private fun solveDay91() {
+/*def extrapolate(array):
+    if all(x == 0 for x in array):
+        return 0
+
+    deltas = [y - x for x, y in zip(array, array[1:])]
+    diff = extrapolate(deltas)
+    return array[-1] + diff
+
+total = 0
+
+for line in open(0):
+    nums = list(map(int, line.split()))
+    total += extrapolate(nums)
+
+print(total)*/
+}
+
+private fun solveDay82() {
+    /*from math import gcd
+
+steps, _, *rest = open(0).read().splitlines()
+
+network = {}
+
+for line in rest:
+    pos, targets = line.split(" = ")
+    network[pos] = targets[1:-1].split(", ")
+
+positions = [key for key in network if key.endswith("A")]
+cycles = []
+
+for current in positions:
+    cycle = []
+
+    current_steps = steps
+    step_count = 0
+    first_z = None
+
+    while True:
+        while step_count == 0 or not current.endswith("Z"):
+            step_count += 1
+            current = network[current][0 if current_steps[0] == "L" else 1]
+            current_steps = current_steps[1:] + current_steps[0]
+
+        cycle.append(step_count)
+
+        if first_z is None:
+            first_z = current
+            step_count = 0
+        elif current == first_z:
+            break
+
+    cycles.append(cycle)
+
+nums = [cycle[0] for cycle in cycles]
+
+lcm = nums.pop()
+
+for num in nums:
+    lcm = lcm * num // gcd(lcm, num)
+
+print(lcm)*/
+}
+
+private fun solveDay81() {
+    /*steps, _, *rest = open(0).read().splitlines()
+
+network = {}
+
+for line in rest:
+    pos, targets = line.split(" = ")
+    network[pos] = targets[1:-1].split(", ")
+
+step_count = 0
+current = "AAA"
+
+while current != "ZZZ":
+    step_count += 1
+    current = network[current][0 if steps[0] == "L" else 1]
+    steps = steps[1:] + steps[0]
+
+print(step_count)*/
+}
+
 private fun solveDay72() {
 /*letter_map = {"T": "A", "J": ".", "Q": "C", "K": "D", "A": "E"}
 
